d8cd2a104173cd14eaad4010cf5e68c47c65b796
FedenevI <fedenevia@yandex.ru>
2024-01-14 21:44:18 +0300

    prv1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,17 @@
-// @todo: Темплейт карточки
+const delFunc = (itemCard) => {
+    itemCard.remove()
+}
 
-// @todo: DOM узлы
+const Cards = (card, delFunc) => {
+    const cardTemplate = document.querySelector('#card-template').content
+    const cardList = document.querySelector('.places__list');
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const buttonDeleteCard = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__image').alt = card.name;
+    cardElement.querySelector('.card__title').textContent = card.name;
+    cardList.append(cardElement);
+    buttonDeleteCard.addEventListener('click', () => delFunc(cardElement))
+}
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
-
-console.log(initialCards)
+initialCards.forEach((card) => Cards(card, delFunc))
